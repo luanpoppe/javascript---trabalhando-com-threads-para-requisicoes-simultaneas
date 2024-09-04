92a1ef41b5ac7a78c4672bf5e533e69c5e9ff5b8
luanpoppe <luanpoppe@gmail.com>
2024-09-03 21:43:18 -0300

    feat: final da parte 02 do curso

---
+++ script/scripts.js
@@ -0,0 +1,36 @@
+const graficoDolar = document.getElementById("graficoDolar");
+
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+// setInterval(() => conectaAPI(), 5000);
+
+async function conectaAPI() {
+  const conecta = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+  );
+  const conectaTraduzido = await conecta.json();
+  console.log("conecta", conecta);
+  console.log("conectaTraduzido", conectaTraduzido);
+}
+
+function gerarHorario() {
+  let data = new Date();
+  let horaro =
+    data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  console.log("horaro", horaro);
+  return horaro;
+}
+
+gerarHorario();
