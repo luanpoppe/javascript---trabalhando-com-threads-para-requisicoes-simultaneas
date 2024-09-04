5b08b862179c0d67318560715ed3adca8c9f0d6f
luanpoppe <luanpoppe@gmail.com>
2024-09-04 20:45:13 -0300

    feat: terminei a parte 03 do curso

--- script/scripts.js
+++ script/scripts.js
@@ -1,28 +1,32 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
 const graficoDolar = document.getElementById("graficoDolar");
 
 const graficoParaDolar = new Chart(graficoDolar, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: [],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "Dólar",
+        data: [],
         borderWidth: 1,
       },
     ],
   },
 });
 
-// setInterval(() => conectaAPI(), 5000);
+setInterval(() => conectaAPI(), 5000);
 
 async function conectaAPI() {
   const conecta = await fetch(
     "https://economia.awesomeapi.com.br/json/last/USD-BRL"
   );
   const conectaTraduzido = await conecta.json();
-  console.log("conecta", conecta);
-  console.log("conectaTraduzido", conectaTraduzido);
+  let tempo = gerarHorario()
+  let valor = conectaTraduzido.USDBRL.ask
+  adicionarDados(graficoParaDolar, tempo, valor)
+  imprimeCotacao("dolar", valor)
 }
 
 function gerarHorario() {
@@ -33,4 +37,12 @@ function gerarHorario() {
   return horaro;
 }
 
-gerarHorario();
+// gerarHorario();
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda)
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados)
+  })
+  grafico.update()
+}
